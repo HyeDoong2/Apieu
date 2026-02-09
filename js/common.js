7bd901c310bca60b9f936e9382c7eeb71a4538af
한혜진 <gkagus98@naver.com>
2026-02-10 00:19:41 +0900

    0210 12:19

--- js/common.js
+++ js/common.js
@@ -19,13 +19,11 @@ $(function () {
   });
 
 
-  // 메뉴 열기
-  $(document).on("click", ".menu_open a", function (e) {
+  $(document).on("click", ".menu_open", function (e) {
     e.preventDefault();
     $(".menu_wrap").addClass("active");
   });
 
-  // 메뉴 닫기
   $(document).on("click", ".quick_menu li:last-child a", function (e) {
     e.preventDefault();
     $(".menu_wrap").removeClass("active");
@@ -34,7 +32,6 @@ $(function () {
     $("#gnb > ul > li > a img").attr("src", "images/icn_plus.png");
   });
 
-  // 아코디언 메뉴
   $(document).on("click", "#gnb > ul > li > a", function (e) {
     e.preventDefault();
 
@@ -53,5 +50,6 @@ $(function () {
     $submenu.slideDown();
     $this.find("img").attr("src", "images/icn_minus.png");
   });
+
 
 });
